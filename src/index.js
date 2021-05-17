db0a3dc6a549705d81eb073bac7ae6c5f876a053
alakhisaran <a.lakhisaran@gmail.com>
2021-05-17 17:40:29 +0200

    Integrate One Call API
    to get daily forecast

--- src/index.js
+++ src/index.js
@@ -53,6 +53,14 @@ function currentLocation(position){
  axios.get(apiUrl).then(showTemperature)
 }
 
+function getForecast(coordinates) {
+console.log(coordinates);
+let apiKey = "d688e0aec682fb0302505306304b61bc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast)
+}
+
 function showTemperature(response) {
 //console.log(response.data);
 document.querySelector("#city").innerHTML = response.data.name;
@@ -66,6 +74,9 @@ document.querySelector("#icon").setAttribute(
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 celsiusTemperature = response.data.main.temp;
+
+getForecast(response.data.coord);
+
 }
 
 
@@ -86,8 +97,8 @@ function showCelsius(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
-
+function displayForecast(response) {
+console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat"];
@@ -147,5 +158,3 @@ celsiusLink.addEventListener("click",showCelsius);
 
 //On load show data of this city be default
 searchCity("Sydney");
-
-displayForecast();
